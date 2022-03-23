5eeb0758d350add865d4673502f22dcde1949288
Vargacz Gergő <78954794+vargaczgergo@users.noreply.github.com>
2022-03-23 10:58:05 +0100

    henger ablak elkészítés

---
+++ henger.py
@@ -0,0 +1,18 @@
+from tkinter import *
+import math
+
+foablak = Tk()
+gomb1 = Button(foablak, text='Kiszámítás')
+gomb1.grid(colum = 3, row = 3)
+foablak.title =('tk')
+sugar = Label(foablak, text='Sugár(cm):')
+sugar.grid(colum = 1, row = 1)
+
+
+
+
+
+
+
+
+foablak.mainloop()
